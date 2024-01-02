1556f0558ff6406455f91a006c35e0103d83608d
Idba1 <moniraislamself@gmail.com>
2024-01-02 12:15:59 +0600

    add switch statements

---
+++ conditional-statements/switch.js
@@ -0,0 +1,20 @@
+var sum = 100;
+switch (sum){
+    case 10:
+    console.log("sum is 10");
+    break
+    case 20:
+    console.log("sum is 20");
+    break
+    case 40:
+    console.log("sum is 40");
+    break
+    case 100:
+    console.log("sum is 100");
+    break
+    case 70:
+    console.log("sum is 70");
+    break
+default:
+    console.log("sum is not found");
+}
