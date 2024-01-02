2fc96f0cf11adbd9efc9296df420959cb617cf6e
Idba1 <moniraislamself@gmail.com>
2024-01-02 12:25:48 +0600

    add date object

---
+++ date-object.js/date.js
@@ -0,0 +1,21 @@
+var myDate = new Date();
+
+console.log(myDate.toTimeString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toDateString());
+console.log(myDate.getDate());
+console.log(myDate.getMonth());
+console.log(myDate.getDay());
+console.log(myDate.getHours());
+console.log(myDate.getMinutes());
+console.log(myDate.getSeconds());
+console.log(myDate.getMilliseconds());
+
+
+
+
+
+
+
+
+
